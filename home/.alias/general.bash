0579448949c187878644cf56183dc2f567a2092c
Fred Hatfull <fhats@yelp.com>
2012-05-15 15:40:19 -0700

    Some git-related niceities

--- home/.alias/general.bash
+++ home/.alias/general.bash
@@ -2,6 +2,12 @@ alias i=ipython
 alias infinigrep='grep -ir'
 alias ls='ls -ah'
 
+# I use git a lot
+alias gut='git'
+alias gt='git'
+alias gti='git'
+alias g='git'
+
 # Pipe input to me to have pretty-printed json-decoded output
 alias nicejson='python -c "import pprint
 import json
